eec611f058f0259077a9c5609646981acd65e6b0
nitya-sama <nityasujitha01@gmail.com>
2025-12-05 19:24:49 -0500

    add solutions

--- SubArrayEqualsK/Solution.java
+++ SubArrayEqualsK/Solution.java
@@ -1,2 +1,30 @@
-package SubArrayEqualsK;public class Solution {
+package SubArrayEqualsK;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+
+        // Prefix sum 0 occurs once
+        prefixCount.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+
+            // Check if a subarray ending here sums to k
+            if (prefixCount.containsKey(currentSum - k)) {
+                count += prefixCount.get(currentSum - k);
+            }
+
+            // Store/update the current prefix sum
+            prefixCount.put(currentSum, prefixCount.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
 }
+
